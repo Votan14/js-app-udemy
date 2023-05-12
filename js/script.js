dbdb9dd87d6ff1df6b8125d64ebd09e51dba96ed
Aleksander <votan14@gmail.com>
2023-05-12 08:49:55 +0200

    add cycles and conditions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 /* eslint-disable indent */
 /* eslint-disable no-mixed-spaces-and-tabs */
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
+str.length - и получить её длину)
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
 'use strict';
 
-const numberOfFilms = +prompt('How films did you watching', ''),
+const numberOfFilms = prompt('How films did you watching', ''),
     personalMovieDB = {
         count: numberOfFilms,
         movies: {},
@@ -11,12 +25,24 @@ const numberOfFilms = +prompt('How films did you watching', ''),
         privat: false
     };
 
-const lastViewedFilm1 = prompt('One of last watched film?', ''),
-    lastFilmRating1 = prompt('Your rating this film?', ''),
-    lastViewedFilm2 = prompt('Last watched film?', ''),
-    lastFilmRating2 = prompt('One of last watched film?', '');
+for (let i = 0; i < personalMovieDB['count']; i++) {   
+    const lastViewedFilm = prompt('One of last watched film?', ''),
+    lastFilmRating = prompt('Your rating this film?', '');
+    if (lastViewedFilm != '' && lastFilmRating != '' && lastViewedFilm != null && lastFilmRating != null && lastViewedFilm.length < 50) {
+        personalMovieDB.movies[lastViewedFilm] = lastFilmRating;  
+    } else {
+        i--;
+    }
+}
 
-personalMovieDB.movies[lastViewedFilm1] = lastFilmRating1;
-personalMovieDB.movies[lastViewedFilm2] = lastFilmRating2;
+if (personalMovieDB.count < 10) {
+    console.log('Few films');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) { 
+    console.log('You are a classic viewer');  
+} else if (personalMovieDB.count > 30) {
+    console.log('You are a film fan'); 
+} else {
+    console.log('Error'); 
+}
 
-console.log(personalMovieDB);
+console.log(personalMovieDB.movies);
